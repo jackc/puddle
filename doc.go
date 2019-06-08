11cab39313c9677f9807fef985e6f44e44ddb2ce
Jack Christensen <jack@jackchristensen.com>
2019-06-08 17:40:51 -0500

    Fix spelling error

--- doc.go
+++ doc.go
@@ -4,7 +4,7 @@
 Puddle is a tiny generic resource pool library for Go that uses the standard
 context library to signal cancellation of acquires. It is designed to contain
 the minimum functionality a resource pool needs that cannot be implemented
-without concerrency concerns. For example, a database connection pool may use
+without concurrency concerns. For example, a database connection pool may use
 puddle internally and implement health checks and keep-alive behavior without
 needing to implement any concurrent code of its own.
 */
